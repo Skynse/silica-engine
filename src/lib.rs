c881b5552a0b91aabcb62451047af2eab78e5ef5
Skynse <prodbyskynse@gmail.com>
2023-11-09 08:51:37 -0600

    push files

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod api;
+pub mod elements;
+pub mod particle;
+pub mod variant;
+pub mod variant_type;
+pub mod world;
---
+++ target/debug/deps/engine-cf9bc0417af132f0.d
@@ -0,0 +1,5 @@
+C:\Users\austi\projects\silica\engine\target\debug\deps\engine-cf9bc0417af132f0.rmeta: src\lib.rs
+
+C:\Users\austi\projects\silica\engine\target\debug\deps\engine-cf9bc0417af132f0.d: src\lib.rs
+
+src\lib.rs:
