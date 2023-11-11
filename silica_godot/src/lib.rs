4a79a850e115bc58b3d977a5fb96fff2be2550c5
Austin A. <prodbyskynse@gmail.com>
2023-11-10 18:03:06 -0600

    update

--- silica_godot/src/lib.rs
+++ silica_godot/src/lib.rs
@@ -6,20 +6,21 @@ struct SilicaExtension;
 unsafe impl ExtensionLibrary for SilicaExtension {}
 
 #[derive(GodotClass)]
-#[class(base=Node2D)]
+#[class(base=Node)]
 struct GDWorld {
     world: World,
     #[base]
-    node2d: Base<Node2D>,
+    node: Base<Node>,
 }
 
 #[godot_api]
-impl Node2DVirtual for GDWorld {
-    fn init(node2d: Base<Node2D>) -> Self {
+impl NodeVirtual for GDWorld {
+    fn init(node: Base<Node>) -> Self {
+        godot_print!("tick");
         let world = World::new(256, 256);
         let image = Image::new();
 
-        GDWorld { world, node2d }
+        GDWorld { world, node: node }
     }
 
     fn physics_process(&mut self, delta: f64) {
@@ -28,5 +29,25 @@ impl Node2DVirtual for GDWorld {
         }
 
         // create image and edit pixels based on world.world.particle value
+    }
+}
+
+impl GDWorld {
+    pub fn get_data(&self) -> PackedByteArray {
+        let mut data = PackedByteArray::new();
+        data.resize(self.world.width as usize * self.world.height as usize * 4);
+
+        for x in 0..self.world.width as usize {
+            for y in 0..self.world.height as usize {
+                let index = (x + y * self.world.width as usize) * 4;
+                let particle = self.world.get_particle(x as i32, y as i32);
+                let color = variant_type(particle.variant).color;
+                data.set(index, color.0);
+                data.set(index + 1, color.1);
+                data.set(index + 2, color.2);
+                data.set(index + 3, 255);
+            }
+        }
+        data
     }
 }
