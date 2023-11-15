6f36ce1c42b19bb5ec160824205219e2e0513a2f
Austin A. <prodbyskynse@gmail.com>
2023-11-15 10:27:11 -0600

    update engine

--- silica_engine/src/prelude.rs
+++ silica_engine/src/prelude.rs
@@ -1,6 +1,6 @@
 // prelude file
 pub use crate::api::*;
-pub use crate::particle::*;
+pub use crate::particle::{particle_to_color, Particle};
 pub use crate::variant::*;
 pub use crate::variant_type::*;
 pub use crate::world::*;
--- silica_engine/src/variant.rs
+++ silica_engine/src/variant.rs
@@ -1,7 +1,7 @@
 use crate::{
     api::API,
     particle::Particle,
-    variant_type::{self, variant_type, VariantProperty, VARIANTS},
+    variant_type::{variant_type, VariantProperty, VARIANTS},
 };
 pub static EMPTY_CELL: Particle = Particle {
     variant: Variant::Empty,
@@ -18,6 +18,36 @@ pub enum Variant {
     Water = 3,
     Fire = 4,
 }
+trait VariantTrait {
+    fn get_variant(&self) -> Variant;
+    fn update(&self, particle: Particle, api: API);
+}
+pub struct Sand;
+
+impl VariantTrait for Sand {
+    fn get_variant(&self) -> Variant {
+        Variant::Sand
+    }
+    fn update(&self, particle: Particle, mut api: API) {
+        let dx = api.rand_dir();
+        let nb = api.get(dx, 1);
+        let nbr = api.get(dx + 1, 1);
+        let nbl = api.get(dx - 1, 1);
+
+        if nb.variant == Variant::Empty {
+            api.set(dx, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nbr.variant == Variant::Empty {
+            api.set(dx + 1, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nbl.variant == Variant::Empty {
+            api.set(dx - 1, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        }
+    }
+}
+
+// create a sand element using trait
 
 impl std::fmt::Display for Variant {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -39,102 +69,101 @@ impl Variant {
         match self {
             Variant::Empty => (),
             Variant::Wall => (),
-            Variant::Sand => update_sand(particle, api),
-            Variant::Water => update_water(particle, api),
-            Variant::Fire => update_fire(particle, api),
+            Variant::Sand => Sand.update(particle, api),
+            Variant::Water => Water.update(particle, api),
+            Variant::Fire => Fire.update(particle, api),
         }
     }
 }
 
-pub fn update_sand(particle: Particle, mut api: API) {
-    let dx = api.rand_dir();
-    let nb = api.get(dx, 1);
-    let nbr = api.get(dx + 1, 1);
-    let nbl = api.get(dx - 1, 1);
+struct Fire;
 
-    if nb.variant == Variant::Empty {
-        api.set(dx, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nbr.variant == Variant::Empty {
-        api.set(dx + 1, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nbl.variant == Variant::Empty {
-        api.set(dx - 1, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
+impl VariantTrait for Fire {
+    fn get_variant(&self) -> Variant {
+        Variant::Fire
     }
-}
 
-pub fn update_fire(particle: Particle, mut api: API) {
-    let dx = api.rand_dir();
-    let nb = api.get(dx, 1);
-    let nbr = api.get(dx + 1, 1);
-    let nbl = api.get(dx - 1, 1);
-    let nl = api.get(-1, 0);
-    let nr = api.get(1, 0);
+    fn update(&self, particle: Particle, mut api: API) {
+        let dx = api.rand_dir();
+        let nb = api.get(dx, 1);
+        let nbr = api.get(dx + 1, 1);
+        let nbl = api.get(dx - 1, 1);
+        let nl = api.get(-1, 0);
+        let nr = api.get(1, 0);
 
-    // check if neighbor is powder property and displace if heavier
-    let nb_type = variant_type(nb.variant);
+        // check if neighbor is powder property and displace if heavier
+        let nb_type = variant_type(nb.variant);
 
-    if nb_type.variant_property == VariantProperty::Powder {
-        if nb_type.weight > VARIANTS[particle.variant as usize].weight {
-            api.set(0, 1, particle);
-            api.set(0, 0, nb);
+        if nb_type.variant_property == VariantProperty::Powder {
+            if nb_type.weight > VARIANTS[particle.variant as usize].weight {
+                api.set(0, 1, particle);
+                api.set(0, 0, nb);
+            }
+        }
+
+        if nb.variant == Variant::Empty {
+            api.set(dx, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nbr.variant == Variant::Empty {
+            api.set(dx + 1, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nbl.variant == Variant::Empty {
+            api.set(dx - 1, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nl.variant == Variant::Empty {
+            api.set(-1, 0, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nr.variant == Variant::Empty {
+            api.set(1, 0, particle);
+            api.set(0, 0, EMPTY_CELL);
         }
     }
-
-    if nb.variant == Variant::Empty {
-        api.set(dx, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nbr.variant == Variant::Empty {
-        api.set(dx + 1, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nbl.variant == Variant::Empty {
-        api.set(dx - 1, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nl.variant == Variant::Empty {
-        api.set(-1, 0, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nr.variant == Variant::Empty {
-        api.set(1, 0, particle);
-        api.set(0, 0, EMPTY_CELL);
-    }
 }
 
-pub fn update_water(particle: Particle, mut api: API) {
-    let dx = api.rand_dir();
-    let nb = api.get(0, 1);
-    let nbr = api.get(dx + 1, 1);
-    let nbl = api.get(dx - 1, 1);
-    let nl = api.get(-1, 0);
-    let nr = api.get(1, 0);
+struct Water;
 
-    // check if neighbor is powder property and displace if heavier
-    let nb_type = variant_type(nb.variant);
+impl VariantTrait for Water {
+    fn get_variant(&self) -> Variant {
+        Variant::Water
+    }
 
-    if nb_type.variant_property == VariantProperty::Powder {
-        if nb_type.weight > VARIANTS[particle.variant as usize].weight {
-            api.set(0, 1, particle);
-            api.set(0, 0, nb);
+    fn update(&self, particle: Particle, mut api: API) {
+        let dx = api.rand_dir();
+        let nb = api.get(0, 1);
+        let nbr = api.get(dx + 1, 1);
+        let nbl = api.get(dx - 1, 1);
+        let nl = api.get(-1, 0);
+        let nr = api.get(1, 0);
+
+        // check if neighbor is powder property and displace if heavier
+        let nb_type = variant_type(nb.variant);
+
+        if nb_type.variant_property == VariantProperty::Powder {
+            if nb_type.weight > VARIANTS[particle.variant as usize].weight {
+                api.set(0, 1, particle);
+                api.set(0, 0, nb);
+            }
+        }
+
+        if nb.variant == Variant::Empty {
+            api.set(dx, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nbr.variant == Variant::Empty {
+            api.set(dx + 1, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nbl.variant == Variant::Empty {
+            api.set(dx - 1, 1, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nl.variant == Variant::Empty {
+            api.set(-1, 0, particle);
+            api.set(0, 0, EMPTY_CELL);
+        } else if nr.variant == Variant::Empty {
+            api.set(1, 0, particle);
+            api.set(0, 0, EMPTY_CELL);
         }
     }
-
-    if nb.variant == Variant::Empty {
-        api.set(dx, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nbr.variant == Variant::Empty {
-        api.set(dx + 1, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nbl.variant == Variant::Empty {
-        api.set(dx - 1, 1, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nl.variant == Variant::Empty {
-        api.set(-1, 0, particle);
-        api.set(0, 0, EMPTY_CELL);
-    } else if nr.variant == Variant::Empty {
-        api.set(1, 0, particle);
-        api.set(0, 0, EMPTY_CELL);
-    }
 }
+
 pub fn particle_to_color(variant: Variant) -> (u8, u8, u8) {
     let res = match variant {
         Variant::Empty => VARIANTS[0].color,
